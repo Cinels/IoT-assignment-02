94a7a2ba727c921bea2fdd5b4de1dc8d3f0f9522
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-19 22:30:43 +0100

    feat(Button): added header and implementation for constructor

---
+++ src/arduino/Smart_Waste_Disposal/include/devices/Button.hpp
@@ -0,0 +1,15 @@
+#ifndef __BUTTON__
+#define __BUTTON__
+
+class Button {
+
+    int pin;
+
+public:
+    Button(int pin);
+    void init();
+};
+
+
+
+#endif
---
+++ src/arduino/Smart_Waste_Disposal/src/devices/Button.cpp
@@ -0,0 +1,9 @@
+#include "devices/Button.hpp"
+
+Button::Button(int pin) {
+    this->pin = pin;
+}
+
+void Button::init() {
+    
+}
