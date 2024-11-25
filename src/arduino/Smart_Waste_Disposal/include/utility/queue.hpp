ac97c6d6d82133a5d9ce6383edfdf2de02de1bfe
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-25 23:24:13 +0100

    feat(queue): added queue implementation

--- src/arduino/Smart_Waste_Disposal/include/utility/queue.hpp
+++ src/arduino/Smart_Waste_Disposal/include/utility/queue.hpp
@@ -1,6 +1,25 @@
 #ifndef __QUEUE__
 #define __QUEUE__
 
+typedef struct Node{
+  struct Node *next;
+  void (*func)();
+} Node;
 
+typedef struct Queue{
+  Node *head;
+  Node *tail;
+  short size;
+} Queue;
+
+Queue* initialize();
+
+void clear(Queue* Q);
+
+void enqueue(Queue* Q, void(*func)());
+
+void dequeue(Queue* Q);
+
+Node* getNext(Queue* Q);
 
 #endif
--- src/arduino/Smart_Waste_Disposal/src/utility/queue.cpp
+++ src/arduino/Smart_Waste_Disposal/src/utility/queue.cpp
@@ -1,2 +1,51 @@
+#include <stdlib.h>
 #include "utility/queue.hpp"
 
+#define MAX_SIZE 10
+
+Queue* initialize() {
+  Queue* Q = (Queue*)malloc(sizeof(Queue));
+  Q->head = NULL;
+  Q->tail = NULL;
+  Q->size = 0;
+  return Q;
+}
+
+void clear(Queue* Q) {
+  while(Q->head != NULL) {
+    dequeue(Q);
+  }
+}
+
+void enqueue(Queue* Q, void(*func)()) {
+  if(Q->size < MAX_SIZE) {
+    Node *temp = (Node*)malloc(sizeof(Node));
+    temp->next = NULL;
+    temp->func = func;
+    if (Q->head == NULL) {
+      Q->head = temp;
+    }
+    if (Q->tail != NULL) {
+      Q->tail->next = temp;
+    }
+    Q->size++;
+    Q->tail = temp;
+  }
+}
+
+void dequeue(Queue* Q) {
+  Node* temp = Q->head;
+  if(temp != NULL && Q->size > 0) {
+    if(Q->head->next == NULL) {
+      Q->head = NULL;
+      Q->tail = NULL;
+    }
+    else Q->head = Q->head->next;
+    Q->size--;
+    free(temp);
+  }
+}
+
+Node* getNext(Queue* Q) {
+  return Q->head;
+}
