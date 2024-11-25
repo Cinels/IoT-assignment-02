63c3bdf91e313cee1ba6211aa2a1e9a3cb530e7a
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-26 00:14:36 +0100

    feat(button): added interrupt attachment

--- src/arduino/Smart_Waste_Disposal/include/devices/Button.hpp
+++ src/arduino/Smart_Waste_Disposal/include/devices/Button.hpp
@@ -1,13 +1,19 @@
 #ifndef __BUTTON__
 #define __BUTTON__
 
+#include "utility/queue.hpp"
+
+typedef enum buttonType {OPEN_BUTTON, CLOSE_BUTTON} buttonType;
+
 class Button {
 private:
     int pin;
-    long prevts;
+    Queue* Q;
+    static void openButtonPressed();
+    static void closeButtonPressed();
 public:
-    Button(int pin);
-    void init(/*open-close, coda*/);
+    Button(int pin, Queue* Q);
+    void init(buttonType type);
 };
 
 #endif
--- src/arduino/Smart_Waste_Disposal/src/devices/Button.cpp
+++ src/arduino/Smart_Waste_Disposal/src/devices/Button.cpp
@@ -1,11 +1,27 @@
 #include "devices/Button.hpp"
+#include "utility/interrupt_utils.hpp"
 #include <Arduino.h>
 
-Button::Button(int pin) {
+Button::Button(int pin, Queue* Q) {
     this->pin = pin;
-    this->prevts = millis();
+    this->Q = Q;
 }
 
-void Button::init() {
-    //associa ad un interrupt
+void Button::init(buttonType type) {
+    switch (type) {
+    case OPEN_BUTTON: setInterrupt(this->pin, openButtonPressed, FALLING); break;
+    case CLOSE_BUTTON: setInterrupt(this->pin, closeButtonPressed, FALLING); break;
+    default: break;
+    }
+    
+}
+
+__attribute__((unused))
+static void openButtonPressed() {
+    //aggiungi in coda il fatto che sei stato premuto
+}
+
+__attribute__((unused))
+static void closeButtonPressed() {
+    //aggiungi in coda il fatto che sei stato premuto
 }
