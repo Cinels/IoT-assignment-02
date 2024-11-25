b65015de8157b827db32432ca60845cb8bbd4bf2
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-25 23:41:33 +0100

    feat(interrupt): added implementation for interrupt utility library

--- src/arduino/Smart_Waste_Disposal/include/utility/interrupt_utils.hpp
+++ src/arduino/Smart_Waste_Disposal/include/utility/interrupt_utils.hpp
@@ -1,6 +1,6 @@
 #ifndef __INTERRUPT_UTILS__
 #define __INTERRUPT_UTLIS__
 
-
+void setInterrupt(uint8_t pin, void (*func)(), uint8_t mode);
 
 #endif
--- src/arduino/Smart_Waste_Disposal/src/utility/interrupt_utils.cpp
+++ src/arduino/Smart_Waste_Disposal/src/utility/interrupt_utils.cpp
@@ -1,3 +1,7 @@
 #include <EnableInterrupt.h>
+#include <Arduino.h>
 #include "utility/interrupt_utils.hpp"
 
+void setInterrupt(uint8_t pin, void (*func)(), uint8_t mode) {
+    enableInterrupt(pin, func, mode);
+}
