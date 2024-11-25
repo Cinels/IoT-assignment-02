bbb5aa8f91088c4a6e93c2117e60a83fa5436898
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-25 17:42:04 +0100

    feat(button): added implementation

--- src/arduino/Smart_Waste_Disposal/src/devices/Button.cpp
+++ src/arduino/Smart_Waste_Disposal/src/devices/Button.cpp
@@ -1,9 +1,11 @@
 #include "devices/Button.hpp"
+#include <Arduino.h>
 
 Button::Button(int pin) {
     this->pin = pin;
+    this->prevts = millis();
 }
 
 void Button::init() {
-    
+    //associa ad un interrupt
 }
