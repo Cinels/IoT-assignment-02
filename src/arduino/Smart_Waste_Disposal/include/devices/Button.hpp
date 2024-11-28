a91df39a142070d23dff2b9cc7023106d74d0799
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-28 19:22:51 +0100

    fix(button): removed queue reference and interrupt handler

--- src/arduino/Smart_Waste_Disposal/include/devices/Button.hpp
+++ src/arduino/Smart_Waste_Disposal/include/devices/Button.hpp
@@ -1,18 +1,13 @@
 #ifndef __BUTTON__
 #define __BUTTON__
 
-#include "utility/queue.hpp"
-
 typedef enum buttonType {OPEN_BUTTON, CLOSE_BUTTON} buttonType;
 
 class Button {
 private:
     int pin;
-    Queue* Q;
-    static void openButtonPressed();
-    static void closeButtonPressed();
 public:
-    Button(int pin, Queue* Q);
+    Button(int pin);
     void init(buttonType type);
 };
 
