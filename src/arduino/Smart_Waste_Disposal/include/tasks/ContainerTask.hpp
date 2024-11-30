355b6a876491c620caab79aafaa7674fdda6e2ed
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-30 22:21:41 +0100

    feat(container task): added implementation for initialization and CLOSE state

--- src/arduino/Smart_Waste_Disposal/include/tasks/ContainerTask.hpp
+++ src/arduino/Smart_Waste_Disposal/include/tasks/ContainerTask.hpp
@@ -1,13 +1,36 @@
 #ifndef __CONTAINER_TASK__
 #define __CONTAINER_TASK__
 
-#include "Task.hpp"
+#include "utility/Flag.hpp"
+#include "tasks/Task.hpp"
+#include "tasks/FillingTask.hpp"
+#include "tasks/TemperatureTask.hpp"
+#include "devices/Button.hpp"
+#include "devices/Led.hpp"
+#include "devices/Display.hpp"
+#include "devices/Door.hpp"
+#include "devices/UserDetector.hpp"
+
+typedef enum containerState {SLEEP, AWAKE, OPEN, CLOSE} containerState;
+
 
 class ContainerTask : public Task {
 private:
-    enum {SLEEP, AWAKE, OPEN, CLOSE} state;
+    containerState state;
+    Flag* tempAllarm;
+    Flag* containerFull;
+    Button* openButton;
+    Button* closeButton;
+    Led* greenLed;
+    Led* redLed;
+    Display* display;
+    Door* door;
+    UserDetector* userDetector;
+    long ts;
 public:
     ContainerTask(int period);
+    void init(Flag* tempflag, Flag* fillflag);
+    void setDevices(Button* openButton, Button* closeButton, Led* greenLed, Led* redLed, Display* display, Door* door, UserDetector* userDetector);
     void tick();
 };
 
--- src/arduino/Smart_Waste_Disposal/src/tasks/ContainerTask.cpp
+++ src/arduino/Smart_Waste_Disposal/src/tasks/ContainerTask.cpp
@@ -1,11 +1,75 @@
 #include "tasks/ContainerTask.hpp"
 #include <Arduino.h>
-#include "tasks/Task.hpp"
+#include <avr/sleep.h>
+
+#define DISPLAY_POSITION 0, 1
+#define DISPLAY_POSITION_2 0, 2
+
+#define SLEEP_TIME 15000
+#define OPEN_TIME 15000
+#define CLOSE_TIME 2000
 
 ContainerTask::ContainerTask(int period) {
     Task::init(period);
+    this->state = AWAKE;
+    this->ts = millis();
+}
+
+void ContainerTask::init(Flag* tempflag, Flag* fillflag) {
+    this->tempAllarm = tempAllarm;
+    this->containerFull = containerFull;
+}
+
+void ContainerTask::setDevices(Button* openButton, Button* closeButton, Led* greenLed, Led* redLed, Display* display, Door* door, UserDetector* userDetector) {
+    this->openButton = openButton;
+    this->closeButton = closeButton;
+    this->greenLed = greenLed;
+    this->redLed = redLed;
+    this->display = display;
+    this->door = door;
+    this->userDetector = userDetector;
 }
 
 void ContainerTask::tick() {
     Serial.println("CONTAINER TASK");
+    switch (this->state) {
+    case AWAKE:
+        if(millis() - ts >= SLEEP_TIME) {
+            this->state = SLEEP;
+            // sleepScreen();
+            set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+            sleep_enable();
+            sleep_mode();  
+
+            sleep_disable();
+            this->state = AWAKE;
+            // old screen
+            this->ts = millis();
+        }
+        //guarda la coda degli eventi (ci stanno dentro l'user sensor, i bottoni e il movimento del motore)
+        // dopo che hai aperto ↓
+        // this->display->setText(DISPLAY_POSITION, "PRESS CLOSE WHEN DONE");
+        // this->ts = millis();
+        break;
+    case OPEN:
+        //guarda la coda
+        if(millis() - ts >= OPEN_TIME) {
+            this->state = CLOSE;
+            this->door->close();
+            this->display->setText(DISPLAY_POSITION, "WASTE RECEIVED");
+            this->ts = millis();
+        }
+        break;
+    case CLOSE:
+        if(millis() - ts >= CLOSE_TIME) {
+            this->state = AWAKE;
+            if(!this->tempAllarm->getFlag() && !this->containerFull->getFlag()) {
+                this->display->setText(DISPLAY_POSITION, "PRESS OPEN TO");
+                this->display->setText(DISPLAY_POSITION_2, "ENTER WASTE");
+            }
+            this->ts = millis();
+        }
+        break;
+    default: break;
+    }
 }
