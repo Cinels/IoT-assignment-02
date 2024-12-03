502dd703a273fc3ab8458b29d12212fa14f6329a
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-12-03 16:16:54 +0100

    feat(led): added doxygen comments

--- src/arduino/Smart_Waste_Disposal/include/devices/Led.hpp
+++ src/arduino/Smart_Waste_Disposal/include/devices/Led.hpp
@@ -3,12 +3,19 @@
 
 #include "interfaces/Light.hpp"
 
+/// @brief Class to manage leds.
 class Led : public Light {
 private:
     int pin;
 public:
+    /// @brief Led constructor, it creates a led.
+    /// @param pin the pin where the led is connected.
     Led(int pin);
+
+    /// @brief Turns on the led.
     void switchOn();
+
+    /// @brief Turns off the led.
     void switchOff();
 };
 
