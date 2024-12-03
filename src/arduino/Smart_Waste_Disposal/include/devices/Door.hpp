266d6b7895ca835c438d57505de27df600813ce7
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-12-03 16:08:21 +0100

    feat(door): added doxygen comments

--- src/arduino/Smart_Waste_Disposal/include/devices/Door.hpp
+++ src/arduino/Smart_Waste_Disposal/include/devices/Door.hpp
@@ -3,12 +3,19 @@
 
 #include "ServoMotor.hpp"
 
-class Door  {
+/// @brief Class to manage a door.
+class Door {
 private:
     ServoMotor* servoMotor;
 public:
+    /// @brief Door constructor, it creates an electric door.
+    /// @param pin the pin where the door is connected to be controlled.
     Door(int pin);
+    
+    /// @brief Opens the door.
     void open();
+    
+    /// @brief Closes the door.
     void close();   
 };
 
