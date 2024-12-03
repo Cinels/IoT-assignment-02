fdc60ddfa04988b0fa0842684c0563150721d273
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-12-03 17:09:01 +0100

    feat(container task): added doxygen comments

--- src/arduino/Smart_Waste_Disposal/include/tasks/ContainerTask.hpp
+++ src/arduino/Smart_Waste_Disposal/include/tasks/ContainerTask.hpp
@@ -11,9 +11,10 @@
 #include "devices/Door.hpp"
 #include "devices/UserDetector.hpp"
 
+/// @brief Enumeration to track the state of the task.
 typedef enum containerState {SLEEP, AWAKE, OPEN, CLOSE} containerState;
 
-
+/// @brief Task to manage a waste container.
 class ContainerTask : public Task {
 private:
     containerState state;
@@ -27,9 +28,26 @@ private:
     UserDetector* userDetector;
     long ts;
 public:
+    /// @brief Task constructor, it creates a task with a base period.
+    /// @param period the base period every which the task must be performed.
     ContainerTask(int period);
+
+    /// @brief Sets the flag object which keeps trak of allarms and instruction to be communicated among theads.
+    /// @param flag the flag to communicate among threads.
     void setFlag(Flag* flag);
+
+    /// @brief Set devices to operate with.
+    /// @param openButton the button to open the container.
+    /// @param closeButton the button to close the container.
+    /// @param greenLed the led that indicates that the container is available.
+    /// @param redLed the led that indicates that the container is not available.
+    /// @param display the display that show to the user messages.
+    /// @param door the door that opens and closes the container.
+    /// @param userDetector the user sensor that perceives if an user is present and wakes up the container.
     void setDevices(Button* openButton, Button* closeButton, Led* greenLed, Led* redLed, Display* display, Door* door, UserDetector* userDetector);
+
+    /// @brief the running method, every time is called it checks if the base time is elapsed and if so, 
+    /// depending on the state of the container performs some actions.
     void tick();
 };
 
