657f6b6783b1a36cf51460b0a3dfa4d25c71c197
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-12-03 16:32:09 +0100

    feat(user detector): added doxygen comments

--- src/arduino/Smart_Waste_Disposal/include/devices/UserDetector.hpp
+++ src/arduino/Smart_Waste_Disposal/include/devices/UserDetector.hpp
@@ -3,13 +3,20 @@
 
 #include "interfaces/PIR.hpp"
 
+/// @brief Class to manage an user detector.
 class UserDetector : public PIR {
 private:
     int pin;
-    static void userDetected();
 public:
+    /// @brief User detector constructor, it creates an user detector. 
+    /// @param pin the pin where the user detector is connected.
     UserDetector(int pin);
+
+    /// @brief Initialize the user detector by calibrating it and attaching an interrupt.
     void init();
+
+    /// @brief Dummy interrupt handler to wake up the system if is in sleep mode. 
+    static void userDetected();
 };
 
 #endif
--- src/arduino/Smart_Waste_Disposal/include/devices/interfaces/PIR.hpp
+++ src/arduino/Smart_Waste_Disposal/include/devices/interfaces/PIR.hpp
@@ -1,8 +1,10 @@
 #ifndef __PIR__
 #define __PIR__
 
+/// @brief Abstract class to use PIR sensors. 
 class PIR {
 public:
+    /// @brief Initialize the sensor.
     virtual void init() = 0;
 };
 
