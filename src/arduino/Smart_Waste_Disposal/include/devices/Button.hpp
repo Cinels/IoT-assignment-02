6d2ab3b5a342aa25d9d75a9ae17ac922c9bc2e33
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-12-03 15:48:49 +0100

    hotFix(button): added a new line between methods

--- src/arduino/Smart_Waste_Disposal/include/devices/Button.hpp
+++ src/arduino/Smart_Waste_Disposal/include/devices/Button.hpp
@@ -9,6 +9,7 @@ public:
     /// @brief Button constructor, it creates a button and sets the pin in INPUT mode.
     /// @param pin the input pin for the button.
     Button(int pin);
+    
     /// @brief Method that returns if the button is pressed.
     /// @return 1 if the button is pressed, 0 otherwise.
     int isPressed();
